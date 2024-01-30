c6c9af7365a9d7e567f73ffc67d8eca7d5b8c4be
NewJumper <muhtasim.nabi@gmail.com>
2024-01-30 16:52:09 -0600

    added a grid

---
+++ index.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector("canvas")
+const context = canvas.getContext("2d")
+
+const tileSize = 32
+const gridSize = 16;
+const padding = 2;
+canvas.width = padding + gridSize * (tileSize + padding)
+canvas.height = padding + gridSize * (tileSize + padding)
+console.log(canvas)
+
+for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
+        context.fillStyle = "black"
+        context.fillRect(padding + i * (tileSize + padding), padding + j * (tileSize + padding), tileSize, tileSize)
+        if(i == 0) context.fillStyle = "red"
+        else context.fillStyle = "white"
+        context.fillText(i + j * gridSize, padding + i * (tileSize + padding) + 3, padding + j * (tileSize + padding) + 12)
+    }
+}
