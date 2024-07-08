725800dfba5b3bac8ad68859bf1bb3123fde099a
NewJumper <muhtasim.nabi@gmail.com>
2024-07-08 14:19:19 -0500

    changed theme switcher icon's file location

--- scripts/toggle-theme.js
+++ scripts/toggle-theme.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', loadTheme);
 function loadTheme() {
     const image = document.getElementById('theme-toggle');
     if (localStorage.getItem('theme') === 'dark') {
-        image.src = "resources/assets/lyrics_guess/light-mode.svg";
+        image.src = "resources/assets/global/light-mode.svg";
         document.body.classList.add('dark-mode');
     }
 }
@@ -12,10 +12,10 @@ function toggleTheme() {
     document.body.classList.toggle('dark-mode');
     const image = document.getElementById('theme-toggle');
     if (document.body.classList.contains('dark-mode')) {
-        image.src = "resources/assets/lyrics_guess/light-mode.svg";
+        image.src = "resources/assets/global/light-mode.svg";
         localStorage.setItem('theme', 'dark');
     } else {
-        image.src = "resources/assets/lyrics_guess/dark-mode.svg";
+        image.src = "resources/assets/global/dark-mode.svg";
         localStorage.setItem('theme', 'light');
     }
 }
